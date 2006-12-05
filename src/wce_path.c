6c2f9be1b0c23ce80b9003bfeb0c0e39a6b4f051
sdunand <sdunand@c6a91760-cd0d-0410-8ae5-d0ab54974c5e>
2006-12-05 08:01:27 +0000

    Added GetFullPathNameW, _fullpath, _wfullpath functions
    git-svn-id: https://wcelibcex.svn.sourceforge.net/svnroot/wcelibcex/trunk@41 c6a91760-cd0d-0410-8ae5-d0ab54974c5e

--- src/wce_path.c
+++ src/wce_path.c
@@ -2,7 +2,7 @@
  * $Id: wce_path.c,v 1.0 2006/11/29 16:56:01 sdunand Exp $
  *
  * Defines _splitpath, _wsplitpath, _makepath, _wmakepath,
- *         _fullpath, _wfullpath functions
+ *         wceex_GetFullPathNameW, _fullpath, _wfullpath functions
  *
  * Created by Stéphane Dunand (sdunand@sirap.fr)
  *
@@ -33,8 +33,20 @@
 
 #include <string.h>
 #include <wce_stdlib.h>
+#include <wce_direct.h>
+#include <wce_errno.h>
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_splitpath
+*
+* Description:
+*    Break a path name into components.
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
 
 void wceex_splitpath( const char *path, 
                       char *drive, char *dir, char *name, char *ext )
@@ -83,7 +95,17 @@ void wceex_splitpath( const char *path,
     }
 }
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_wsplitpath
+*
+* Description:
+*    Break a path name into components.
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
 
 void wceex_wsplitpath( const wchar_t *path, 
                        wchar_t *drive, wchar_t *dir, wchar_t *name, wchar_t *ext )
@@ -132,7 +154,17 @@ void wceex_wsplitpath( const wchar_t *path,
     }
 }
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_makepath
+*
+* Description:
+*    Create a path name from components
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
 
 void wceex_makepath( char *path,
                      const char *drive, const char *dir,
@@ -192,7 +224,17 @@ void wceex_makepath( char *path,
     }
 }
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_wmakepath
+*
+* Description:
+*    Create a path name from components
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
 
 void wceex_wmakepath( wchar_t *path,
                       const wchar_t *drive, const wchar_t *dir,
@@ -252,22 +294,186 @@ void wceex_wmakepath( wchar_t *path,
     }
 }
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_GetFullPathNameW
+*
+* Description:
+*    retrieves the full path and file name of a specified file.
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
 
-char* wceex_fullpath( char *absPath, const char *relPath, size_t maxLength )
+DWORD wceex_GetFullPathNameW( LPCWSTR lpFileName, DWORD nBufferLength, 
+                              LPWSTR lpBuffer, LPWSTR *lpFilePart )
 {
-    *absPath = 0;
-    strncat( absPath, relPath, maxLength - 1 );
-    return absPath;
+    int up = 0, down = 0;
+    size_t len_tot, len_buf = 0;
+    LPWSTR file;
+
+    // reference to current working directory ?
+    if( wcsncmp( lpFileName, L".\\", 2 ) == 0 )
+        down = 1;
+    else if( wcsncmp( lpFileName, L"./", 2 ) == 0 )
+        down = 2;
+    if( wcsncmp( lpFileName, L"..\\", 3 ) == 0 )
+        up = 1;
+    else if( wcsncmp( lpFileName, L"../", 3 ) == 0 )
+        up = 2;
+    if( down || up )
+    {
+        LPWSTR last;
+        len_buf = wceex_GetCurrentDirectoryW( nBufferLength, lpBuffer );
+        if( !len_buf )
+            return 0;
+        // backslash at the end ?
+        last = lpBuffer + len_buf - 1;
+        if( *last != '\\' && *last != '/' )
+        {
+            // test sufficient buffer before add
+            len_buf++;
+            if( len_buf >= nBufferLength )
+                return len_buf + wcslen( lpFileName ) + 1;
+            last++;
+            if( down == 1 || up == 1 )
+                *last = '\\';
+            else
+                *last = '/';
+            *(last + 1) = 0;
+        }
+        if( down )
+        {
+            lpBuffer = last + 1;
+            lpFileName += 2;
+        }
+        else if( up )
+        {
+            LPWSTR fname = (LPWSTR)lpFileName;
+            for(;;)
+            {
+                // root ?
+                if( last == lpBuffer )
+                {
+                    errno = ERROR_BAD_PATHNAME;
+                    return 0;
+                }
+                // erase last backslash
+                *last = 0;
+                // parent directory
+                if( up == 1 )
+                    last = wcsrchr( lpBuffer, '\\' );
+                else
+                    last = wcsrchr( lpBuffer, '/' );
+                if( !last )
+                {
+                    errno = ERROR_BAD_PATHNAME;
+                    return 0;
+                }
+                *(last + 1) = 0;
+                // next parent directory ?
+                fname += 3;
+                if( up == 1 )
+                {
+                    if( wcsncmp( fname, L"..\\", 3 ) )
+                        break;
+                }
+                else
+                {
+                    if( wcsncmp( fname, L"../", 3 ) )
+                        break;
+                }
+            }
+            len_buf = wcslen( lpBuffer );
+            lpBuffer = last + 1;
+            lpFileName = fname;
+        }
+    }
+    len_tot = len_buf + wcslen( lpFileName );
+    if( len_tot >= nBufferLength )
+        return len_tot + 1;
+    wcscpy( lpBuffer, lpFileName );
+    // delimiter of file name ?
+    file = wcsrchr( lpBuffer, '\\' );
+    if( !file )
+        file = wcsrchr( lpBuffer, '/' );
+    if( file )
+    {
+        file++;
+        if( *file == 0 )
+            *lpFilePart = NULL;
+        else
+            *lpFilePart = file;
+    }
+    else
+        *lpFilePart = lpBuffer;
+    return len_tot;
 }
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_wfullpath
+*
+* Description:
+*    Create an absolute or full path name for the specified relative path name.
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
 
 wchar_t* wceex_wfullpath( wchar_t *absPath, const wchar_t *relPath, size_t maxLength )
 {
-    *absPath = 0;
-    wcsncat( absPath, relPath, maxLength - 1 );
+    wchar_t* lpFilePart;
+    DWORD ret = wceex_GetFullPathNameW( relPath, maxLength, absPath, &lpFilePart );
+    if( !ret || ret > maxLength )
+    {
+        *absPath = 0;
+        return NULL;
+    }
     return absPath;
 }
 
-//---------------------------------------------------------------------------
+/*******************************************************************************
+* wceex_fullpath
+*
+* Description:
+*    Create an absolute or full path name for the specified relative path name.
+*
+* Return:
+*
+* Reference:
+*
+*******************************************************************************/
+
+char* wceex_fullpath( char *absPath, const char *relPath, size_t maxLength )
+{
+    wchar_t wrelPath[_MAX_PATH*2], *wabsPath, *wret;
+	if( !MultiByteToWideChar( CP_ACP, 0, relPath, -1, wrelPath, _MAX_PATH*2 ) )
+    {
+        errno = ENOMEM;
+        *absPath = 0;
+        return NULL;
+    }
+    if( (wabsPath = (wchar_t*)malloc( maxLength * sizeof(wchar_t) )) == NULL )
+    {
+        errno = ENOMEM;
+        *absPath = 0;
+        return NULL;
+    }
+    wret = wceex_wfullpath( wabsPath, wrelPath, maxLength ); 
+    if( wret && !WideCharToMultiByte( CP_ACP, 0, wabsPath, -1, absPath, 
+                                      maxLength, NULL, NULL ) )
+    {
+        errno = GetLastError();
+        wret = NULL;
+    }
+    free( wabsPath );
+    if( !wret )
+    {
+        *absPath = 0;
+        return NULL;
+    }
+    return absPath;
+}
